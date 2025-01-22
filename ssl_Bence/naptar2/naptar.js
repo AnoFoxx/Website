80089238057401667d4fa1ce63c1a8e44fe9dffa
Papp Bence <89926378+Papp-Bence-Attila@users.noreply.github.com>
2025-01-22 09:55:11 +0100

    Delete ssl_Bence directory
    :(

--- ssl_Bence/naptar2/naptar.js
+++
@@ -1,8 +0,0 @@
-const szamozottNaptarNap = document.getElementsByClassName("naptar-szamozott-nap");
-
-
-for (let i = 0; i < szamozottNaptarNap.length; i++) {
-	szamozottNaptarNap[i].addEventListener("mouseover", e => {
-		console.log(e);
-	});	
-}
