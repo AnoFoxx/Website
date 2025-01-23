f8780e6062667ab5972c13a0b6c615d8b9476600
Papp Bence <89926378+Papp-Bence-Attila@users.noreply.github.com>
2025-01-23 12:27:10 +0100

    Add files via upload
    foglalni kívánt napok kiválasztása és a kiválasztás átnyúlása más hónapra

--- naptar/index.php
+++ naptar/index.php
@@ -11,5 +11,12 @@
 </head>
 <body>
 	<iframe src="naptar.php"></iframe>
+	<script type="text/javascript">
+		sessionStorage.setItem("starterDate", null);
+		sessionStorage.setItem("endingDate", null);
+		setInterval(()=>{
+			console.log(sessionStorage.getItem("starterDate"), sessionStorage.getItem("endingDate"))
+		}, 1000);
+	</script>
 </body>
 </html>
