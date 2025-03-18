35b7d1613fc0974368a8c540d392f00e6bfd0341
Papp Bence <89926378+Papp-Bence-Attila@users.noreply.github.com>
2025-03-18 10:58:07 +0100

    Update index.php

--- Website_temp/index.php
+++ Website_temp/index.php
@@ -50,6 +50,7 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 	<script src="scripts/js/img_load.js"></script>
 	<script src="scripts/js/main.js"></script>
+	<script src="scripts/js/nyelv.js"></script>
 	<title>ChillOut</title>
 <script>
 
