2c0af5e7f1e0e2edeffc194da52baea7c18f0db8
Hőzső Attila Pál <149316703+AnoFoxx@users.noreply.github.com>
2025-03-18 10:52:11 +0100

    Update index.php

--- Website_temp/index.php
+++ Website_temp/index.php
@@ -64,12 +64,11 @@
 				<span id="ikon">&#9776;</span>
 			</div>
 			<div id="fo_menu" style="display:none;" class="absol">
-				<div class="menu relat flex a-i-center wid" id="menu_fooldal">Főoldal</div>
-				<div class="menu relat flex a-i-center wid" id="menu_apartmanok">Apartmanok</div>
-				<div class="menu relat flex a-i-center wid" id="menu_informacio">Informáicók</div>
-				<div class="menu relat flex a-i-center wid" id="menu_galeria">Galéria</div>
-				<div class="menu relat flex a-i-center wid" id="menu_ertekeles">Értékelések</div>
-				<div class="menu relat flex a-i-center wid" id="menu_stb">Stb</div>
+				<div class="menu relat flex a-i-center wid" id="menu_fooldal" data-key="menu-fooldal">Főoldal</div>
+				<div class="menu relat flex a-i-center wid" id="menu_apartmanok" data-key="menu-apartmanok">Apartmanok</div>
+				<div class="menu relat flex a-i-center wid" id="menu_informacio" data-key="menu-informacio">Informáicók</div>
+				<div class="menu relat flex a-i-center wid" id="menu_galeria" data-key="menu-galeria">Galéria</div>
+				<div class="menu relat flex a-i-center wid" id="menu_ertekeles" data-key="menu-ertekeles">Értékelések</div>
 			</div>
 		</div>
 			
@@ -78,8 +77,8 @@
 		<div class="foscroll relat wid hei-vh">
 			<div id="fooldal" class="scroll_pont flex j-center a-i-center relat wid hei-vh">
 				<div style="display: flex;justify-content: center;align-items: center;z-index: 1;position: absolute;width: 50%;height: 20vh;background-color: rgba(255, 255, 255, 0.7);filter: blur(10px); border-radius:50px;"></div>
-				<div id="bemutat_hatter">
-					<p id="bemutat" class="flex j-center a-i-center absol">Üvözlünk a ChillOut apartman weboldalán</p>
+				<div id="udvozles_hatter">
+					<p id="udvozles" class="flex j-center a-i-center absol" data-key="udvozles">Üvözlünk a ChillOut apartman weboldalán</p>
 				</div>
 				<img id="fo_kep" class="wid-vw hei-vh" draggable="false" src="static/images/chillout_tinodi_outside.jpg">
 				<header id="menu_sor" class="flex a-i-center absol wid">
@@ -117,7 +116,7 @@
 						
 						<div id="foglal" class="hover_item menu_pont_hatter flex j-center a-i-center">
 							<div class="menu_pont_foglalas flex j-center a-i-center">
-								<span>Foglalás</span>
+								<span data-key="foglalas">Foglalás</span>
 							</div>
 						</div>
 						
@@ -151,7 +150,7 @@
 
 								<div class="apartman_div_leir hei accent_hide">
 									<div style="width:100%;height: 50%;">
-										<span>blu blu</span>
+										<span data-key="tinodi_leir">blu blu</span>
 									</div>
 								</div>
 							</div>
@@ -197,7 +196,7 @@
 
 								<div class="apartman_div_leir hei accent_hide">
 									<div style="width:100%;height: 50%;">
-										<span>blu blu</span>
+										<span data-key="chill_leir">blu blu</span>
 									</div>
 								</div>
 							</div>
@@ -223,7 +222,7 @@
 				</div>
 
 				<div id="apartman_garazs_szoba" class="apartman flex j-center">
-					<div class="felirat flex j-center a-i-center wid">Chill Apartman + 1 szoba</div>
+					<div class="felirat flex j-center a-i-center wid" data-key="chillapart_felirat">Chill Apartman + 1 szoba</div>
 					<div class="apartman_div wid hei">
 						<div id="garazs_szoba_div" class="apartman_div_tav flex a-i-center hei">
 							<div class="apartman_div_box flex  relat wid">
@@ -261,11 +260,11 @@
 				<img src="static/images/cool-background_2.png" class="wid hei absol" style="z-index:0;">
 				<div id="belso_eler" style="z-index:1;" class="flex wid">
 					<div id="info_felirat" class="flex j-center a-i-center wid">
-						<h1 style="color: rgba(255, 255, 255, 0.8); font-size:60px;">Az apartmanokról</h1>
+						<h1 style="color: rgba(255, 255, 255, 0.8); font-size:60px;" data-key="info_felirat">Az apartmanokról</h1>
 					</div>
 					<div id="informaciok" class="flex wid">
 						<div id="teszt_1" class="flex j-center a-i-center hei">
-							<div id="leiras">
+							<div id="leiras" data-key="ossz_informacio">
 								bla bla bla bla bla bla
 							</div>
 						</div>
@@ -294,7 +293,7 @@
 					<img src="static/images/cool-background.png" class="wid hei absol" style="filter: blur(2px);">
 					<div id="galeria_body" class="j-center a-i-center flex">
 						<div id="galeria_felirat" class="j-center a-i-center flex wid">
-							<h1 style="font-size: 50px;color:black;">Galéria</h1>
+							<h1 style="font-size: 50px;color:black;" data_key="galeria">Galéria</h1>
 						</div>
 						<div id="galeria_scroll" class="flex" style="z-index:2;">
 							<?php 
@@ -361,7 +360,7 @@
 			</div>
 			<div id="ertekeles" class="scroll_pont flex wid hei-vh">
 				<div class="flex j-center a-i-center" style="width: 100%; height: 100%; flex-wrap: wrap;">
-					<h1>Értékelések</h1>
+					<h1 data-key="ertekeles">Értékelések</h1>
 					<div id="ertekeles_body" class="flex  a-c-center j-center wid">
 						
 						<div id="ertekeles_sor" class="flex j-center a-i-center wid">
