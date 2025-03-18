6265f55a0e43d43453220c46ca205a6c752c63f1
Papp Bence <89926378+Papp-Bence-Attila@users.noreply.github.com>
2025-03-18 10:55:58 +0100

    Add files via upload

---
+++ Website_temp/scripts/js/nyelv.js
@@ -0,0 +1,35 @@
+let nyelv = "en";
+
+const loadLang = lang => {
+    fetch(`static/json/${lang}.json`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            updateTextFromJson(data);
+        })
+        .catch(error => {
+            console.error(`Fetch error: ${error}`);
+        });
+    
+};
+
+const updateTextFromJson = jsonData => {
+	const elements = document.querySelectorAll('[data-key]');
+	
+	elements.forEach(element => {
+		const key = element.getAttribute('data-key');
+		if (jsonData[key]) {
+			element.textContent = jsonData[key];
+		}
+	});
+}
+
+window.onload = e => {
+    console.log("nyelv betölés folyamatban");
+	loadLang("en");
+    console.log("nyelv betöltve");
+}
