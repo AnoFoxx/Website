e49c173cd6a575e03357db6633652abc5ed37105
Hőzső Attila Pál <149316703+AnoFoxx@users.noreply.github.com>
2025-03-19 12:01:59 +0100

    Add files via upload

---
+++ public/scripts/js/carousel.js
@@ -0,0 +1,21 @@
+const reviews = document.querySelectorAll('.ertekeles_elem');
+const nextButton = document.getElementById('next_review');
+const prevButton = document.getElementById('prev_review');
+const container = document.getElementById('ertekeles_sor');
+let currentIndex = 0;
+
+function updateCarousel() {
+    container.style.transform = `translateX(-${currentIndex * 640}px)`;
+}
+
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % reviews.length;
+    updateCarousel();
+});
+
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + reviews.length) % reviews.length;
+    updateCarousel();
+});
+
+updateCarousel();
