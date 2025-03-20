24688753d84bc42c9d920a991e1d463690d437b6
Hőzső Attila Pál <149316703+AnoFoxx@users.noreply.github.com>
2025-03-20 08:49:28 +0100

    Update img_load.js

--- public/scripts/js/img_load.js
+++ public/scripts/js/img_load.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
-    let betoltottImg = 0;
+	let betoltottImg = 0;
 
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                const img = entry.target;
-                const src = $(img).data('src'); 
-                
-                setTimeout(() => {
-                    $(img).attr('src', src).addClass('betoltott');
-                }, betoltottImg * 100); 
-                betoltottImg++;
+	const observer = new IntersectionObserver((entries, observer) => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				const img = entry.target;
+				const src = $(img).data('src'); 
+				
+				setTimeout(() => {
+						$(img).attr('src', src).addClass('betoltott');
+				}, betoltottImg * 100); 
+				betoltottImg++;
 
-                observer.unobserve(img); 
-            }
-        });
-    }, { threshold: 0.1}); 
+				observer.unobserve(img); 
+			}
+		});
+	}, { threshold: 0.1}); 
 
-    $('.lazy_toltes').each(function() {
-        observer.observe(this);
-    });
+	$('.lazy_toltes').each(function() {
+			observer.observe(this);
+	});
 });
