afa7aae5c81e35379eccf4e6bee0737f9e0c6910
Hőzső Attila Pál <149316703+AnoFoxx@users.noreply.github.com>
2025-03-23 16:48:47 +0100

    Add files via upload

---
+++ public/confirm.php
@@ -0,0 +1,26 @@
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Confirm</title>
+ 	<link rel="stylesheet" type="text/css" href="static/css/confirm_style.css">
+ </head>
+ <body>
+ 	<header>
+ 		<div style="background-color: rgba(0,0,0,0.5);width: 100%;height: 100%;position: absolute;"></div>
+ 		<div style="width:100%;height: 100%;z-index: 2;display: flex;align-items: center;justify-content: center;">
+ 			<div id="logo" style="background-color: transparent;">
+			<div style="background-image: url(static\\images\\madar_logo.svg);background-color: none;background-repeat: no-repeat;background-position: center;background-size: cover;width: 100%;height: 100%;transform: rotate(20deg) scaleX(-1);"></div>
+			</div>
+			<div id="felirat" class="j-center a-i-center">ChillOut.hu</div>
+ 		</div>
+ 	</header>
+ 	<div style="width:100%;height: calc(100% - 100px);position: fixed;margin-top: 100px;display: flex;justify-content: center;align-items: center;">
+ 		<div style="width:50%;height: 80%;border-radius: 50px; border: 2px solid grey;background-color: rgba(0,0,0,0.3);display: flex;justify-content: space-evenly;align-items: center;flex-direction: column;">
+ 			<span class="szoveg">Köszönjük, hogy a ChillOut-ot választotta!<br>
+ 			Foglalása sikeresen megerősítésre került.</span>
+ 			<span class="szoveg">Thank you for choosing ChillOut!<br>
+ 			Your booking has been successfully confirmed.</span>
+ 		</div>
+ 	</div>
+ </body>
+ </html>
