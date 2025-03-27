47690a0a25cb0706f88e78d7c0ceb2d3fef3c626
Hőzső Attila Pál <149316703+AnoFoxx@users.noreply.github.com>
2025-03-27 12:19:42 +0100

    Update foglalas.php

--- public/foglalas.php
+++ public/foglalas.php
@@ -7,13 +7,20 @@
 	<link rel="stylesheet" type="text/css" href="static/css/foglalas_style.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 	<script src="scripts/js/foglalas.js"></script>
+	<script src="scripts/js/nyelv.js"></script>
 </head>
 <body>
 	<div id="menu_hatter" style="display:none; "></div>
 	<div id="menu" style="display: none;">
 		<div id="iksz"></div>
-		<div style="margin-top:100px;width: 100%;height: calc(100% - 100px);justify-content: center;align-content: center; display: flex;">
-			<div class="menu_pont_design"><input type="checkbox" name="dark_mode" id="dark_mode"><span>Sötét Mód</span></div>
+		<div style="margin-top:100px;width: 100%;height: calc(100% - 100px);display: flex;flex-direction: column;">
+			<div class="menu_pont_design"><input type="checkbox" name="dark_mode" id="dark_mode"><label for="dark_mode">Sötét Mód / Dark Mode</label></div>
+			<div class="lang-container menu_pont_design">
+				<div style="display:flex;align-items: center;">
+					<div style="width: 40px;height: 40px;display: flex;justify-content: center;align-items: center;"><img src="static/images/Hungary.png" class="flag"></div>
+					<span style="color:white;">Nyelv változtatása / Change language</span>
+				</div>
+			</div>
 		</div>
 	</div>
 	<header>
@@ -33,7 +40,7 @@
 	<div id="full" style="overflow-x: scroll;">
 		<form action="save.php" method="post" enctype="multipart/form-data">
 			<div style="width:100%;height:70px;display:flex;justify-content: center;">
-				<h1>Foglalás</h1>
+				<h1 data-key="foglalas">Foglalás</h1>
 			</div>
 
 			<div id="info_div">
@@ -68,54 +75,54 @@
 						}
 					</script>
 
-					<div class="option">
-						Vezetéknév:
+					<div class="option" >
+						<span data-key="vezeteknev">Vezetéknév:</span>
 						<input type="text" name="vez-name" required>
 					</div>
-					<div class="option">
-						Utónév:
+					<div class="option" >
+						<span data-key="utonev">Utónév:</span>
 						<input type="text" name="uto-name" required>
 					</div>
 					<div class="option">
 						Email:
 						<input type="email" name="email" placeholder="youremail@gmail.com" required></div>
 					<div class="option">
-						Telefonszám:
+						<span data-key="telefonszam">Telefonszám:</span>
 						<input type="tel" name="phone-number" required></div>
-					<div class="option">
-						Ország:
+					<div class="option" >
+						<span data-key="orszag">Ország:</span>
 						<input type="text" name="orszag" required></div>
 					<div class="option">
-						Ir.szám:
+						<span data-key="iranyitoszam">Irányítószám:</span>
 						<input type="text" name="irsz" required></div>
 					<div class="option">
-						Település:
+						<span data-key="telepules">Település:</span>
 						<input type="text" name="telepules" required></div>
 					<div class="option">
-						Utca, házszám:
+						<span data-key="utca-hazszam">Utca, házszám:</span>
 						<input type="text" name="lakcim" required>
 					</div>
 					<div style="display:flex; justify-content: space-evenly;">
 						<div class="age">
-							Felnőtt:
+							<span data-key="felnott">Felnőtt:</span>
 							<input max="15" min="1" class="szemely" type="number" name="felnott" required>
 						</div>
 						<div class="age">
-							Gyerek:
+							<span data-key="gyerek">Gyerek:</span>
 							<input max="15" class="szemely" type="number" name="gyerek" required>
 						</div>
 					</div>
 					<div style="display:flex; justify-content:center;align-items: center;">
-						Apartman:
+						<span data-key="apartman">Apartman:</span>
 						<select id="apartman_select">
-							<option id="default_option" value="0">--Válasz apartmant--</option>
-							<option id="apartman_1" value="1">Tinódi apartman</option>
-							<option id="apartman_2" value="2">Chill apartman</option>
+							<option id="default_option" value="0" data-key="def-apartman-option">--Válasz apartmant--</option>
+							<option id="apartman_1" value="1" data-key="apartman-1">Tinódi apartman</option>
+							<option id="apartman_2" value="2" data-key="apartman-2">Chill apartman</option>
 						</select>
 						<input type="hidden" name="apartman" id="apartman">
 					</div>
 					<div class="option-plus">
-						Üzenet:<br>
+						<span data-key="uzenet">Üzenet:</span><br>
 						<textarea class="uzenet"></textarea>
 					</div>
 				</div>
